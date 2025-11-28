a33d38f09563d52c3281a18350661e040cd79e3f
Jeff <jmartinbaldwin@gmail.com>
2025-11-28 10:43:43 +0900

    write initial working version-needs refactoring

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,17 @@
 def bubble_sort(array)
-  
-  
+  for i in 0...array.length
+      for j in (i+1)...array.length
+          if array[i] > array[j]
+            array[i], array[j] = array[j], array[i]
+          end
+      end
+  end  
+array
 end
 
 array = [4,3,78,2,0,2]
+
+p bubble_sort(array)
+
+
+
