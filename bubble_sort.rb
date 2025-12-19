d5ec84ceda154b2331cbc355529b555141d802a0
Jeff <jmartinbaldwin@gmail.com>
2025-12-19 12:24:42 +0900

    Add RuboCop setup and change code to RuboCop suggestions

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,14 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
-  for i in 0...array.length
-      for j in 0...(array.length-1)
-          if array[j] > array[j+1]
-            array[j], array[j+1] = array[j+1], array[j]
-          end
-      end
-  end  
-array
+  (0...array.length).each do |_|
+    (0...(array.length - 1)).each do |j|
+      array[j], array[j + 1] = array[j + 1], array[j] if array[j] > array[j + 1]
+    end
+  end
+  array
 end
 
-array = [4,3,78,2,0,2]
+array = [4, 3, 78, 2, 0, 2]
 
 p bubble_sort(array)
-
-
-
